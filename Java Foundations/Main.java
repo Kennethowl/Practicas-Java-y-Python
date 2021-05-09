59f00c09d30c219bd75ad97c9179a1be72744f92
Kennethowl <kennethowl00@gmail.com>
2021-05-09 02:24:51 -0600

    Unidad 4

---
+++ Java Foundations/Main.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+    	 String greeting = "Java World!";
+         String w = greeting.substring(7, 11);
+         System.out.println(w);
+	}
+}
