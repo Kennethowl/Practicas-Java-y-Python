badad138d5399a1d16baa33860c835a3d82ea62c
Kennethowl <kennethowl00@gmail.com>
2021-05-17 00:48:14 -0600

    pruebas de servers

--- Python/serverproof.py
+++ Python/serverproof.py
@@ -1,18 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return  'CASTLEVANIA'
+    return render_template("home.html")
 
 @app.route('/about')
 def about():
     return 'I belong to Jesus, my guide in this life'
 
-@app.route('/prueba')
-def prueba():
-    return 'Estamos haciendo otra prueba'
+@app.route('/form')
+def form():
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(debug = True, port = 8000)
